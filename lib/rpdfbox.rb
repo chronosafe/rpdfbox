3ead2a28261ff04df33451e16d11c111ca1797b6
Richard Brown <chronosafe@gmail.com>
2016-04-27 11:22:07 -0400

    Update rpdfbox.rb
    Updating version

--- lib/rpdfbox.rb
+++ lib/rpdfbox.rb
@@ -2,5 +2,5 @@ require 'fileutils'
 Dir[File.expand_path(File.dirname(__FILE__)) + "/rpdfbox/*.rb"].each{|f| require f}
 # Location of our PDF Box executable jar
 module RPDFBox
-  $PDFBOX_EXECUTABLE = File.expand_path(File.dirname(__FILE__)) + "/jars/pdfbox-app-1.7.1.jar"
+  $PDFBOX_EXECUTABLE = File.expand_path(File.dirname(__FILE__)) + "/jars/pdfbox-app-2.0.1.jar"
 end
